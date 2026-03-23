88642f6ffca3df24e2310e18c7b4e8ced7b58cf2
DC-VIETIS\BacTH <bacth>
2026-03-23 17:18:51 +0700

    update code

---
+++ DemoECommerce.ProductApiSolution/ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,33 @@
+﻿using eCommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+
+namespace ProductApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService (this IServiceCollection services, IConfiguration config)
+        {
+            // Add database connectivity
+            // Add authentication scheme
+            SharedServiceContainer.AddSharedLibrary<ProductDbContext>(services, config, config["MySerilog:FileName"]!);
+
+            // Create DI
+            services.AddScoped<IProduct, ProductRepository>();
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            //Register  middleware such as
+            // Global Exception: Handle external errors
+            // Listen to Only Api Gateway
+            SharedServiceContainer.UseSharedPolicies(app);
+            return app;
+        }
+    }
+}
